c6adef6177eaed4af696d375eecb309f34eaf3b7
Ponnam Sandeep <101128910+PonnamSandeep@users.noreply.github.com>
2022-11-07 07:38:43 +0530

    patterns

---
+++ Pattern.java
@@ -0,0 +1,25 @@
+package patternsrec;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		triangle(4,0);
+
+	}
+	
+	static void triangle(int r,int c) {
+		if(r==0) {
+			return;
+		}
+		if(c<r) {
+			System.out.print("*");
+			triangle(r,c+1);
+//			System.out.print("*");
+		}else {
+			System.out.println();
+			triangle(r-1,0);
+//			System.out.println();
+		}
+	}
+
+}
